930fd56cc4119008ba8915916d76190938e6cbaa
Jorge L. Garcia <jlg.sftdev@gmail.com>
2020-03-21 12:27:54 -0700

    Modified Makefile's 'clean' rule to remove exec directory created on macOS

--- c/trees/bst/main.c
+++ c/trees/bst/main.c
@@ -6,16 +6,17 @@ int main(int argc, char **argv)
   bintree_t *bt;
   init(&bt);
 
-  node_t *n1 = new_node(21, "root right");
-  node_t *n2 = new_node(17, "root left");
-  node_t *n3 = new_node(18, "root left right");
-  node_t *n4 = new_node(20, "root right left");
+  node_t * n0 = new_node(19, "root");
+  node_t * n1 = new_node(21, "root right");
+  node_t * n2 = new_node(18, "root left");
+  node_t * n3 = new_node(17, "root left left");
+  node_t * n4 = new_node(20, "root right left");
 
-  insert(bt, new_node(19, "root"));
+  insert(bt, n0);
   insert(bt, n1);
+  insert(bt, n2);
   insert(bt, n3);
   insert(bt, n4);
-  insert(bt, n2);
 
   print(bt, PRE);
   print(bt, IN);
